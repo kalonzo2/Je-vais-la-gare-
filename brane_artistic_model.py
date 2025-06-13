44055c8895473b442b252097e1a54191c2492b44
kalonzo2 <k.alonzo268@icloud.com>
2025-06-13 15:03:30 -0600

    Update brane_artistic_model.py

--- brane_artistic_model.py
+++ brane_artistic_model.py
@@ -4,10 +4,12 @@ import trimesh
 # CREATE BOX
 def create_box():
     vertices = np.array([
-        [0, 0, 7],
-        [-7, 50, 3.5],
-        [7, -7, 0]])
-    ])
+        [0, 0, 0],
+        [-7, 50, 7],
+        [7, -7, -7],
+        [10, 0, 0],
+        [
+        ])
     faces = np.array([[0, 1, 2]])
     return trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
 
